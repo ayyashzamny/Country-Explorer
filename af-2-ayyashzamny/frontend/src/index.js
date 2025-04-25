101ab25f948841988d509f2fac9711b93fc9121d
Ayyash <ayyashzamny2002@gmail.com>
2025-04-26 00:01:03 +0530

    test with API key

--- af-2-ayyashzamny/frontend/src/index.js
+++ af-2-ayyashzamny/frontend/src/index.js
@@ -7,8 +7,9 @@ import { BrowserRouter } from 'react-router-dom';
 import { ClerkProvider } from '@clerk/clerk-react';
 
 
-const clerkFrontendApi = "pk_test_dHJ1c3R5LXB1Zy0zNS5jbGVyay5hY2NvdW50cy5kZXYk";
-
+// const clerkFrontendApi = "pk_test_dHJ1c3R5LXB1Zy0zNS5jbGVyay5hY2NvdW50cy5kZXYk";
+const clerkFrontendApi =process.env.REACT_APP_CLERK_PUBLISHABLE_KEY;
+ 
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
