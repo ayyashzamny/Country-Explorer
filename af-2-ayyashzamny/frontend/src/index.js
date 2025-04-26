45733a4fe7f3ffe02aa5afa20c777257e7e48c35
Ayyash <ayyashzamny2002@gmail.com>
2025-04-26 11:26:32 +0530

    new deploymnet

--- af-2-ayyashzamny/frontend/src/index.js
+++ af-2-ayyashzamny/frontend/src/index.js
@@ -7,8 +7,8 @@ import { BrowserRouter } from 'react-router-dom';
 import { ClerkProvider } from '@clerk/clerk-react';
 
 
-// const clerkFrontendApi = "pk_test_dHJ1c3R5LXB1Zy0zNS5jbGVyay5hY2NvdW50cy5kZXYk";
-const clerkFrontendApi =process.env.REACT_APP_CLERK_PUBLISHABLE_KEY;
+const clerkFrontendApi = "pk_test_dHJ1c3R5LXB1Zy0zNS5jbGVyay5hY2NvdW50cy5kZXYk";
+// const clerkFrontendApi =process.env.REACT_APP_CLERK_PUBLISHABLE_KEY;
  
 
 
